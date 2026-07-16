30d391325974c8958d92a70d09da78882f0ef2a2
Brian <brianfleiderman@gmail.com>
2026-07-16 16:43:00 -0400

    typo

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -8,7 +8,7 @@ import socket  # For hostname and IP address
 # Set the page configuration (must be the first Streamlit command)
 st.set_page_config(
     page_title="House Price Predictor",
-    layout="wide",|
+    layout="wide",
     initial_sidebar_state="collapsed"
 )
 
